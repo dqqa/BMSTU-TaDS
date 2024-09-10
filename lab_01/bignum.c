4832a5876982828e8b4ca2aa25ec401a0d6aaf3a
Nikita <zhni23u172@student.bmstu.ru>
2024-09-10 18:26:50 +0300

    Broke number calculation

--- lab_01/bignum.c
+++ lab_01/bignum.c
@@ -3,7 +3,10 @@
 #include "str.h"
 #include <assert.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <string.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 int bignum_parse(bignum_t *num, const char *str)
 {
@@ -12,6 +15,7 @@ int bignum_parse(bignum_t *num, const char *str)
     size_t new_size = strlen(str);
     size_t i_mantissa = 0, i_exp = 0;
     size_t e_count = 0, point_count = 0;
+    bool starting_with_zero = false;
     if (!new_size)
         return ERR_IO;
 
@@ -58,14 +62,19 @@ int bignum_parse(bignum_t *num, const char *str)
         if (str[i_exp] > '9' || str[i_exp] < '0')
             return ERR_NUMBER;
 
+        uint8_t digit = str[i_exp] - '0';
         if (!point_count)
         {
-            num->mantissa_whole[num->mantissa_whole_size] = str[i_exp] - '0';
-            num->mantissa_whole_size++;
+            if (!i_exp && digit == 0)
+                starting_with_zero = true;
+
+            num->mantissa_whole[num->mantissa_whole_size] = digit;
+            if (!starting_with_zero)
+                num->mantissa_whole_size++;
         }
         else
         {
-            num->mantissa_frac[num->mantissa_frac_size] = str[i_exp] - '0';
+            num->mantissa_frac[num->mantissa_frac_size] = digit;
             num->mantissa_frac_size++;
         }
     }
@@ -81,23 +90,27 @@ int bignum_parse(bignum_t *num, const char *str)
 
 int bignum_normalize(bignum_t *num)
 {
-    size_t whole_count = 0, frac_count = 0;
-    for (size_t i = 0; i < num->mantissa_whole_size; i++)
-        if (num->mantissa_whole[i])
-            whole_count++;
-    for (size_t i = 0; i < num->mantissa_frac_size; i++)
-        if (!num->mantissa_frac[i])
-            frac_count++;
+    size_t whole_count = num->mantissa_whole_size, frac_leading_count = 0, frac_ending_count = 0;
 
-    memcpy(num->mantissa_frac, num->mantissa_frac + frac_count,
-           sizeof(uint8_t) * (num->mantissa_frac_size - frac_count));
-    memcpy(num->mantissa_frac + whole_count, num->mantissa_frac, sizeof(uint8_t) * whole_count);
+    for (size_t i = 0; i < num->mantissa_frac_size; i++)
+    {
+        if (!num->mantissa_frac[i])
+            frac_leading_count++;
+        else
+            break;
+    }
+
+    memcpy(num->mantissa_frac + whole_count, num->mantissa_frac, sizeof(uint8_t) * num->mantissa_frac_size);
     memcpy(num->mantissa_frac, num->mantissa_whole, sizeof(uint8_t) * whole_count);
 
-    num->mantissa_whole_size = 0;
-    num->mantissa_frac_size -= frac_count;
     num->mantissa_frac_size += whole_count;
-    num->exponent += whole_count - frac_count;
+
+    for (long i = num->mantissa_frac_size - 1; i >= 0 && !num->mantissa_frac[i]; i--)
+        frac_ending_count++;
+
+    num->mantissa_whole_size = 0;
+    num->mantissa_frac_size -= frac_ending_count;
+    num->exponent += whole_count;
 
     return ERR_OK;
 }
@@ -115,14 +128,86 @@ void bignum_print(const bignum_t *num)
 
 int bignum_validate(const bignum_t *num);
 
+void shift_array_right(uint8_t *arr, size_t size, size_t amount)
+{
+    memmove(arr + amount, arr, sizeof(*arr) * size);
+    memset(arr, 0, sizeof(*arr) * amount);
+}
+
 int bignum_multiply(const bignum_t *num1, const bignum_t *num2, bignum_t *result)
 {
-    memcpy(result, num1, sizeof(*result));
-    result->exponent += num2->exponent - 1;
-    result->sign *= num2->sign;
+    int rc = ERR_OK;
+    memcpy(result, num2, sizeof(*result));
+
+    memset(result->mantissa_frac, 0, sizeof(result->mantissa_frac));
+    memset(result->mantissa_whole, 0, sizeof(result->mantissa_whole));
+
+    result->exponent += num1->exponent - 1;
+    result->sign *= num1->sign;
+
+    size_t max_shift = 0;
+    int remainder = 0;
+    for (long i = num2->mantissa_frac_size - 1; i >= 0; i--)
+    {
+        remainder = 0;
+        for (long j = num1->mantissa_frac_size - 1; j >= 0; j--)
+        {
+            /////////////////////////////////////////////////////////////////////////////////
+            //////////// Определить когда необходимо делать сдвиг и на сколько //////////////
+            /////////////////////////////////////////////////////////////////////////////////
+            
+            if ((i - j) < 0 && ABS(i - j) > max_shift)
+            {
+                max_shift = ABS(i - j);
+                shift_array_right(result->mantissa_frac, result->mantissa_frac_size, ABS(i - j));
+                result->mantissa_frac_size+=ABS(i-j);
+                // result->exponent++; // (?)
+            }
+
+            result->mantissa_frac[i - (num1->mantissa_frac_size - j - 1) + max_shift] += num2->mantissa_frac[i] * num1->mantissa_frac[j] + remainder;
+            remainder = result->mantissa_frac[i - (num1->mantissa_frac_size - j - 1) + max_shift] / 10;
+
+            result->mantissa_frac[i - (num1->mantissa_frac_size - j - 1) + max_shift] %= 10;
+        }
+
+        if (remainder)
+        {
+            shift_array_right(result->mantissa_frac, result->mantissa_frac_size, 1);
+
+            // TODO: Check if remainder > 10
+            result->mantissa_frac[0] = remainder;
+            result->mantissa_frac_size++;
+            if (i != 0)
+                result->exponent++; // (?)
+            max_shift++;
+        }
+    }
+
+    if ((rc = bignum_normalize(result)) != ERR_OK)
+        return rc;
 
     return ERR_OK;
 }
 
 // -123e-41
 // -0.00123
+
+/*
+1.12e123
+10.5e15
+
+0.1e1
+0.2e2
+
+1000
+1
+
+100.01
+0.1
+
+-131.e156
+831.4
+
+99
+99
+*/
--- lab_01/main.c
+++ lab_01/main.c
@@ -9,12 +9,14 @@ int main(void)
     char num1_buf[MAX_INPUT_BUF_SIZE];
     char num2_buf[MAX_INPUT_BUF_SIZE];
     int rc = ERR_OK;
-
+    printf("Input first big number: ");
     if ((rc = str_input(stdin, num1_buf, sizeof(num1_buf))) != ERR_OK)
     {
         print_err(rc);
         return rc;
     }
+
+    printf("Input second big number: ");
     if ((rc = str_input(stdin, num2_buf, sizeof(num2_buf))) != ERR_OK)
     {
         print_err(rc);
@@ -25,17 +27,30 @@ int main(void)
     size_t new_size2 = str_strip(num2_buf);
 
     bignum_t bignum_1, bignum_2, result;
-    rc = bignum_parse(&bignum_1, num1_buf);
-    printf("RC: %d\n", rc);
 
-    rc = bignum_parse(&bignum_2, num2_buf);
+    if ((rc = bignum_parse(&bignum_1, num1_buf)) != ERR_OK)
+    {
+        print_err(rc);
+        return rc;
+    }
+
     printf("RC: %d\n", rc);
+    printf("Num 1 parsed: ");
+    bignum_print(&bignum_1);
+
+    if ((rc = bignum_parse(&bignum_2, num2_buf)) != ERR_OK)
+    {
+        print_err(rc);
+        return rc;
+    }
+
+    printf("RC: %d\n", rc);
+    printf("Num 2 parsed: ");
+    bignum_print(&bignum_2);
 
     bignum_multiply(&bignum_1, &bignum_2, &result);
 
-    bignum_print(&bignum_1);
-    bignum_print(&bignum_2);
-
+    printf("Result: ");
     bignum_print(&result);
 
     return ERR_OK;
