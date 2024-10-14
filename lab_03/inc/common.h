273333f5c2fef1a22d17a1c3784f815474ed04d8
Nikita <zhni23u172@student.bmstu.ru>
2024-10-14 16:17:46 +0300

    Code cleanup & change of default el type

--- lab_03/inc/common.h
+++ lab_03/inc/common.h
@@ -7,9 +7,9 @@
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
-#define DATA_TYPE uint32_t
-#define DATA_PRI PRIu32
-#define DATA_SCN SCNu32
+#define DATA_TYPE int32_t
+#define DATA_PRI PRId32
+#define DATA_SCN SCNd32
 #define DATA_CMP_EQ(a, b) ((a) == (b))
 
 typedef int (*mat_getter_t)(const void *mat, size_t i, size_t j, DATA_TYPE *dst);
@@ -22,7 +22,6 @@ typedef struct __mat_common
     mat_setter_t setter;
 } mat_common_t;
 
-// TODO somehow get number of rows/columns of some matrix. Done ?
 int mat_multiply(const void *mat1, mat_common_t mat1_props, const void *mat2, mat_common_t mat2_props, void *result, mat_common_t result_props);
 int mat_print(FILE *fp, const void *mat, mat_common_t mat_props);
 
