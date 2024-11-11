3fdff51d5b6c50b0a591542d0b1bd881ae40625c
Nikita <zhni23u172@student.bmstu.ru>
2024-11-11 20:44:26 +0300

    Added some temp measures

--- lab_06/src/main.c
+++ lab_06/src/main.c
@@ -1,7 +1,10 @@
+#define _GNU_SOURCE
+
 #include "errors.h"
 #include "file_reader.h"
 #include "tree.h"
 #include <stdio.h>
+#include <time.h>
 
 #define return_defer(num) \
     do                    \
@@ -19,25 +22,43 @@ int main(void)
 {
     tree_t *t = NULL;
     int rc = ERR_OK, result = ERR_OK;
+    FILE *fp = NULL;
+    size_t cnt = 0;
+    struct timespec start, end;
 
     rc = file_load_ex("tests/1.txt", &t);
     if (rc != ERR_OK)
         return_defer(rc);
 
-    tree_to_graphviz(stdout, "tree", t);
+    // tree_to_graphviz(stdout, "tree", t);
 
     printf("tree_search_symbol:\n");
-    size_t cnt = 0;
-    tree_search_symbol(t, '0', &cnt);
-    printf("COUNT: %zu\n\n", cnt);
 
-    printf("file_search_symbol_ex:\n");
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    tree_search_symbol(t, '0', &cnt);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+    double time = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
+    printf("COUNT: %zu; time: %.2f us\n\n", cnt, time);
+
     cnt = 0;
-    file_search_symbol_ex("tests/1.txt", '0', &cnt);
-    printf("COUNT: %zu\n\n", cnt);
+    printf("file_search_symbol_ex:\n");
+    fp = fopen("tests/1.txt", "r");
+    if (!fp)
+        return_defer(ERR_IO);
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    file_search_symbol(fp, '0', &cnt);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+    time = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
+    printf("COUNT: %zu; time: %.2f us\n\n", cnt, time);
 
     defer:
     tree_free(t);
+
+    if (fp)
+        fclose(fp);
 
     return result;
 }
