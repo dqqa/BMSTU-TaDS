0fcc51db98a2438d042c6f43dabc11fe64ad7f52
Nikita <zhni23u172@student.bmstu.ru>
2024-11-19 09:09:36 +0300

    Code cleanup

--- lab_06/src/main.c
+++ lab_06/src/main.c
@@ -19,55 +19,10 @@
 int perf_cmp(void);
 int test_tree(void);
 
-int main2(void)
-{
-    tree_t *t = NULL;
-    int rc = ERR_OK, result = ERR_OK;
-    FILE *fp = NULL;
-    size_t cnt = 0;
-    struct timespec start, end;
-
-    rc = file_load_ex("tests/1.txt", &t);
-    if (rc != ERR_OK)
-        return_defer(rc);
-
-    // tree_to_graphviz(stdout, "tree", t);
-
-    printf("tree_search_symbol:\n");
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    tree_search_symbol(t, '0', &cnt);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
-    double time = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
-    printf("COUNT: %zu; time: %.2f us\n\n", cnt, time);
-
-    cnt = 0;
-    printf("file_search_symbol_ex:\n");
-    fp = fopen("tests/1.txt", "r");
-    if (!fp)
-        return_defer(ERR_IO);
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    file_search_symbol(fp, '0', &cnt);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
-    time = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
-    printf("COUNT: %zu; time: %.2f us\n\n", cnt, time);
-
-    defer:
-    tree_free(t);
-
-    if (fp)
-        fclose(fp);
-
-    return result;
-}
-
 void print_guide(void)
 {
     printf("Программа для построения двоичного дерева поиска из слов в файле.\n"
-           "Предупреждение: в текстовом файле должно быть одно слово на отдельной строке.\n");
+           "Предупреждение: в текстовом файле должно быть одно слово на каждой строке.\n");
 }
 
 int main(void)
@@ -120,6 +75,7 @@ int main(void)
         else if (op == MAIN_MENU_SEARCH)
         {
             char symbol;
+            printf("Введите символ для поиска: ");
             if (scanf("%c", &symbol) != 1)
             {
                 rc = ERR_IO;
