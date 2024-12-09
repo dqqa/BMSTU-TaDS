4eb3a2cf57e8f3c11c114db258cae977118bc275
Nikita <zhni23u172@student.bmstu.ru>
2024-12-09 19:19:32 +0300

    Add time measurements

--- lab_08/src/main.c
+++ lab_08/src/main.c
@@ -2,6 +2,7 @@
 #include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -11,6 +12,7 @@ int main(int argc, char **argv)
         return ERR_ARGS;
     }
 
+    struct timespec start, end;
     graph_t *graph = NULL;
     int **dist = NULL;
     size_t vertices = 0;
@@ -28,14 +30,21 @@ int main(int argc, char **argv)
         dist[i] = (int *)malloc(vertices * sizeof(int));
     }
 
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
     // Выполняем алгоритм Флойда-Уоршелла
     floyd_warshall(graph, dist);
 
     // Находим лучший город
     size_t best_city = find_best_city(dist, vertices);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+    float t = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
+
     printf("Лучший город: %zu\n", best_city);
 
     printf("Использование памяти графом: %zu байт\n", graph_calc_ram_usage(graph));
+    printf("Время поиска: %.2f мкс.\n", t);
 
     generate_graphviz(graph, best_city, "graph2.gp");
     rc = graph_render_open("graph2.gp", "graph2.png");
